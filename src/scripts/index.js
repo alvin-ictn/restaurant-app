e63c4fb499e69011d84ff1720e2af2205caba7ca
Alvin Mantovani <alvin.ictn@gmail.com>
2020-11-30 23:50:07 +0700

    revert back

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,7 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
 import './effect/heroimage'
 
+import './effect/navbar'
+
+
 console.log('initial file! :)');
